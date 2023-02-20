8443f9cb63af48b5df19e3c437c36cfd35f3ac10
AleCordi <cordiolia@gmail.com>
2023-02-20 23:08:37 +0100

    Added Internet_connection script

---
+++ Scripts/Internet_connection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import requests
+import datetime
+url = "https://www.google.it/"
+timeout = 20
+try:
+	request = requests.get(url, timeout=timeout)
+	content=request.content
+	size=len(content)
+	elapsed=request.elapsed
+	print("Connected to the Internet")
+	print("Elapsed time: " + str(elapsed))
+	print("Request status code: " + str(request.status_code))
+	print("Byte size: " + str(size))
+except (requests.ConnectionError, requests.Timeout) as exception:
+	print("No internet connection.")
