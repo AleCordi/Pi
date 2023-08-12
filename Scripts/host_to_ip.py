167b4f0356fa781b68fd0f0f99d4764bd96c1b50
AleCordi <cordiolia@gmail.com>
2023-08-12 21:32:13 +0200

    Delete host_to_ip.py script

--- Scripts/host_to_ip.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#Open file hostname in a list
-with open('/Users/alemacco/Desktop/list/server_list.txt') as f:
-    servers = f.read().splitlines()
-    print(servers)
-for server in servers:
-    server.split(".")[0]
-    print(server)
